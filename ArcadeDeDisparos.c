5db4b7b71e67716a60a383c9f1800ea93ed79998
Júlia Veríssimo <126585562+xjuveri@users.noreply.github.com>
2023-11-22 18:42:21 -0300

    Código pronto

--- ArcadeDeDisparos.c
+++ ArcadeDeDisparos.c
@@ -6,131 +6,119 @@
 #include <string.h>
 #include <unistd.h>
 
-#define VEL_TIROS_INIMIGOS 1 //velocidade dos tiros
-#define INTERVALO_TIRO 100 //intervalo dos tiros em ms
-#define CORACAO "\u2764" //simbolo de coracao com unicode
-#define VIDA_INICIAL 3 //vida inicial do jgdr
-#define MAX_TIROS_INIMIGOS 20 //qnts tiros os inimigos tem no max
-#define LNAVE 5 //largura da nave
-#define ANAVE 1 //altura. usados p calcular colisoes
+#define VEL_TIROS_INIMIGOS 1 
+#define INTERVALO_TIRO 100 
+#define CORACAO "\u2764" 
+#define VIDA_INICIAL 3 
+#define MAX_TIROS_INIMIGOS 20 
+#define LNAVE 5
+#define ANAVE 1 
 #define ESQUERDA 'a' 
 #define DIREITA 'd' 
-#define LINIMIGO 3 //msm coisa da nave.
+#define LINIMIGO 3 
 #define AINIMIGO 1
-#define MAX_INIMIGOS 5 //o max de inimigos
-#define LTIRO 1 //largura e altura dos tiros p colisoes
+#define MAX_INIMIGOS 5 
+#define LTIRO 1 
 #define ATIRO 1
-#define MAX_TIROS 6 //max de balas ativas do jgdr
+#define MAX_TIROS 6 
 
 enum TipoInimigo {INIMIGO_UNICO};
 
-typedef struct Node {
+typedef struct Node{
     char nome[4];
     int pontos;
     struct Node *next;
 } Node;
-
-Node *listaPontuacao = NULL;
-
-typedef struct{ //definicao das balas inimigas e suas coordenadas (x, y)
+typedef struct{
   int Iniciado; 
   int x, y;
 } TiroDosInimigos;
-typedef struct { 
+typedef struct{ 
     int x, y;
     int vida;
     int Iniciado;
-    enum TipoInimigo Tipo; // Adicionei o membro Tipo à estrutura Enemy
+    enum TipoInimigo Tipo; 
 } Enemy;
-typedef struct{ //balas do jgdr e suas coordenadas
+typedef struct{ 
   int Iniciado;
   int x, y;
 } Bullet;
-typedef struct{ //outros objetos e suas coordenadas
+typedef struct{ 
   int x, y;
 } Objeto;
-typedef struct{ //info do jg, como os niveis e total de inimigos
+typedef struct{ 
   int Nivel;
   int TotalInimigos;
 } InfoDoJG;
 
-TiroDosInimigos TirosInimigos[MAX_TIROS_INIMIGOS]; //array com tam max do total de tiros inimigos
-InfoDoJG InfoJG; //guarda info do jg
-Objeto Nave; //representa a nave do jgdr
-Enemy Inimigos[MAX_INIMIGOS]; //array com tam max do total de inimigos
-Bullet Tiros[MAX_TIROS]; //array com tam max do total de tiros
+Node *listaPontuacao = NULL;
+TiroDosInimigos TirosInimigos[MAX_TIROS_INIMIGOS];
+InfoDoJG InfoJG; 
+Objeto Nave; 
+Enemy Inimigos[MAX_INIMIGOS]; 
+Bullet Tiros[MAX_TIROS]; 
 int VidaJgdr=3;
-int Direcao=1; //1/positivo p direita, -1/negativo p esquerda
+int Direcao=1; 
 int Score=0;
 
-void adicionarPontuacao(char nome[4], int pontos) {
+void adicionarPontuacao(char nome[4], int pontos){
     Node *novoNode = (Node *)malloc(sizeof(Node));
     strcpy(novoNode->nome, nome);
     novoNode->pontos = pontos;
     novoNode->next = NULL;
-
-    if (listaPontuacao == NULL) {
+    if (listaPontuacao == NULL){
         listaPontuacao = novoNode;
     } else {
         Node *atual = listaPontuacao;
         Node *anterior = NULL;
-
         while (atual != NULL && pontos < atual->pontos) {
             anterior = atual;
             atual = atual->next;
         }
-
-        if (anterior == NULL) {
+        if (anterior == NULL){
             novoNode->next = listaPontuacao;
             listaPontuacao = novoNode;
-        } else {
+        } else{
             anterior->next = novoNode;
             novoNode->next = atual;
         }
     }
 }
-void mostrarRanking() {
-    printf("\nRanking:\n");
-
+void mostrarRanking(){
+    printf("\n\033[1;34mRanking:\033[m\n");
     Node *atual = listaPontuacao;
     int posicao = 1;
 
     while (atual != NULL && posicao <= 3) {
-        printf("%d. %s - %d pontos\n", posicao, atual->nome, atual->pontos);
+        printf("%d. %s \033[1;34m- %d pontos\033[m\n", posicao, atual->nome, atual->pontos);
         atual = atual->next;
         posicao++;
     }
 }
-void salvarPontuacaoEmArquivo() {
+void salvarPontuacaoEmArquivo(){
     FILE *arquivo = fopen("pontuacao.dat", "wb");
     if (arquivo == NULL) {
-        fprintf(stderr, "Erro ao abrir o arquivo para escrita.\n");
         exit(EXIT_FAILURE);
     }
-
     Node *atual = listaPontuacao;
-
     while (atual != NULL) {
         fwrite(atual, sizeof(Node), 1, arquivo);
         atual = atual->next;
     }
-
     fclose(arquivo);
 }
 
-void carregarPontuacaoDoArquivo() {
+void carregarPontuacaoDoArquivo(){
     FILE *arquivo = fopen("pontuacao.dat", "rb");
     if (arquivo != NULL) {
         Node buffer;
-
         while (fread(&buffer, sizeof(Node), 1, arquivo) == 1) {
             adicionarPontuacao(buffer.nome, buffer.pontos);
         }
-
         fclose(arquivo);
     }
 }
-void liberarListaPontuacao() {
+void liberarListaPontuacao(){
     Node *atual = listaPontuacao;
     while (atual != NULL) {
         Node *proximo = atual->next;
@@ -138,16 +126,15 @@ void liberarListaPontuacao() {
         atual = proximo;
     }
 }
-
 void IniciarTirosInimigos(){
-  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){ // garante q os tiros dos inimigos estejam desativados
-    TirosInimigos[i].Iniciado = 0;              //até a inicialização destes
+  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){ 
+    TirosInimigos[i].Iniciado = 0;             
     TirosInimigos[i].x = -1;
     TirosInimigos[i].y = -1;
   }
 }
 void DesignTirosInimigos(){
-  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){  //percorre os tiros dos inimigos ativos e os desenha na tela (x, y)
+  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){  
     if (TirosInimigos[i].Iniciado){              
       screenGotoxy(TirosInimigos[i].x, TirosInimigos[i].y);
       printf("\033[1;33m!\033m");
@@ -155,7 +142,7 @@ void DesignTirosInimigos(){
   }
 }
 void PercursoTirosInimigos(){
-  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){ //faz a animação dos tiros e desativa os q atingem a parte inferior da tela
+  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){ 
     if (TirosInimigos[i].Iniciado){
       TirosInimigos[i].y += VEL_TIROS_INIMIGOS;
       if (TirosInimigos[i].y >= MAXY){
@@ -165,7 +152,7 @@ void PercursoTirosInimigos(){
   }
 }
 void TiroInimigo(){
-  for (int i = 0; i < MAX_INIMIGOS; ++i){ // verifica os inimigos na tela e ativa os tiros q n estao em uso
+  for (int i = 0; i < MAX_INIMIGOS; ++i){ 
     if (Inimigos[i].Iniciado){
       for (int j = 0; j < MAX_TIROS_INIMIGOS; ++j){
         if (!TirosInimigos[j].Iniciado){
@@ -178,59 +165,53 @@ void TiroInimigo(){
     }
   }
 }
-void drawBorders(){ //funcao da biblioteca de tiaguinho
+void drawBorders(){ 
   screenSetColor(CYAN, BLACK);
   screenBoxEnable();
   screenBoxEnable();
   screenGotoxy(MINX, MINY);
   printf("%c", BOX_UPLEFT);
-
   for (int i = MINX + 1; i < MAXX; ++i) {
     screenGotoxy(i, MINY);
     printf("%c", BOX_HLINE);
   }
-
   screenGotoxy(MAXX, MINY);
   printf("%c", BOX_UPRIGHT);
-
   for (int i = MINY + 1; i < MAXY; ++i) {
     screenGotoxy(MINX, i);
     printf("%c", BOX_VLINE);
     screenGotoxy(MAXX, i);
     printf("%c", BOX_VLINE);
   }
-
   screenGotoxy(MINX, MAXY);
   printf("%c", BOX_DWNLEFT);
-
   for (int i = MINX + 1; i < MAXX; ++i) {
     screenGotoxy(i, MAXY);
     printf("%c", BOX_HLINE);
   }
-
   screenGotoxy(MAXX, MAXY);
   printf("%c", BOX_DWNRIGHT);
   screenBoxDisable();
 }
-void DesignVidaJgdr(){  //desenha na tela a vida do jgdr e a ajusta, conforme ele perde vida
+void DesignVidaJgdr(){  
   screenGotoxy(MINX + 1, MINY + 1);
   printf("  \033[1;34mVida: \033[m");
   for (int i=0; i<VidaJgdr; ++i) {
-    printf("\033[0;31m%s\033[m", CORACAO); //unicode
+    printf("\033[0;31m%s\033[m", CORACAO); 
     printf(" ");
   }
 }
-void IniciarJogo(int Nivel) {
+void IniciarJogo(int Nivel){
   InfoJG.Nivel = Nivel;
   InfoJG.TotalInimigos = MAX_INIMIGOS * (Nivel + 1);
   Nave.x = (MAXX - LNAVE) / 2;
   Nave.y = MAXY - ANAVE - 1;
 
   for (int i = 0; i < MAX_INIMIGOS; ++i) {
-    Inimigos[i].Iniciado = 0; // Desativa todos os inimigos antes de reativar para o novo nível
+    Inimigos[i].Iniciado = 0; 
   }
   for (int i = 0; i < MAX_INIMIGOS; ++i) {
-    Inimigos[i].x = i * (LINIMIGO + 4) + 1; // Posicionamento linear dos inimigos
+    Inimigos[i].x = i * (LINIMIGO + 4) + 1; 
     Inimigos[i].y = 2;
     Inimigos[i].Iniciado = 1;
   }
@@ -245,8 +226,8 @@ void IniciarJogo(int Nivel) {
     TirosInimigos[i].y = -1;
   }
 }
-int InimigosDerrotados(){ //percorre o array de inimigos e verifica se tem algum ativo (Inimigos[i].Iniciado)
-  for (int i=0; i<MAX_INIMIGOS; ++i){ //se encontrar, retorna 0, se nao, retorna 1
+int InimigosDerrotados(){ 
+  for (int i=0; i<MAX_INIMIGOS; ++i){ 
     if (Inimigos[i].Iniciado){
       return 0;
     }
@@ -255,28 +236,27 @@ int InimigosDerrotados(){ //percorre o array de inimigos e verifica se tem algum
 }
 int IniciadoInimigos(){
   int count = 0;
-  for (int i=0; i<MAX_INIMIGOS; ++i){ //percorre o array de inimigos e restorna a quantidade deles que estao em campo
+  for (int i=0; i<MAX_INIMIGOS; ++i){ 
     if (Inimigos[i].Iniciado){
       count++;
     }
   }
   return count;
 }
-void DesignNave(){ //desenha na tela a nave do jgdr (x, y)
+void DesignNave(){ 
   screenGotoxy(Nave.x, Nave.y);
   printf("\033[1;34m⢀⡴⣿⢦⡀\033[m");
   DesignVidaJgdr();
 }
-void DesignInimigos() {
+void DesignInimigos(){
     for (int i = 0; i < MAX_INIMIGOS; ++i) {
         if (Inimigos[i].Iniciado && Inimigos[i].Tipo == INIMIGO_UNICO) {
             screenGotoxy(Inimigos[i].x, Inimigos[i].y);
             printf("\033[1;33m⢈⢝⠭⡫⡁\033[m");
-            // Aqui você desenha o inimigo de acordo com suas coordenadas (Inimigos[i].x, Inimigos[i].y)
         }
     }
 }
-void DesignTiros(){ //desenha os tiros do jgdr
+void DesignTiros(){ 
   for (int i=0; i<MAX_TIROS; ++i){
     if (Tiros[i].Iniciado){
       screenGotoxy(Tiros[i].x, Tiros[i].y);
@@ -284,7 +264,7 @@ void DesignTiros(){ //desenha os tiros do jgdr
     }
   }
 }
-void PercursoNave(int movimento){ //mantem a nave dentro dos limites da tela (movimentacao horizontal)
+void PercursoNave(int movimento){ 
   Nave.x += movimento;
   if (Nave.x < MINX){
     Nave.x = MINX;
@@ -292,19 +272,19 @@ void PercursoNave(int movimento){ //mantem a nave dentro dos limites da tela (mo
     Nave.x = MAXX - LNAVE;
   }
 }
-void PercursoInimigos(){ //movimentacao dos inimigos
-  static int count = 0; //usada para controlar o movimento horizontal
-  count++; //a cada chamada da função, o valor de count é incrementado
+void PercursoInimigos(){ 
+  static int count = 0; 
+  count++; 
   for (int i=0; i<MAX_INIMIGOS; ++i){ 
-    if (Inimigos[i].Iniciado){ //verifica se o inimigo está iniciado
-      Inimigos[i].x += Direcao; //move p esquerda ou direita
-      if (count % 10 == 0){ //sendo múltiplo de 10, se movem verticalmente
+    if (Inimigos[i].Iniciado){
+      Inimigos[i].x += Direcao; 
+      if (count % 10 == 0){ 
         Inimigos[i].y += Direcao;
       }
-      if (Inimigos[i].x + LINIMIGO >= MAXX || Inimigos[i].x <= MINX){ //verifica se algum inimigo atingiu as bordas horizontais da tela
-        Direcao *= -1; // Inverte a direção horizontal
-        for (int j=0; j<MAX_INIMIGOS; ++j){ // Move todos os inimigos para baixo após a inversao
-          Inimigos[j].y += 1; // Movimento vertical após mudar de direção
+      if (Inimigos[i].x + LINIMIGO >= MAXX || Inimigos[i].x <= MINX){ 
+        Direcao *= -1; 
+        for (int j=0; j<MAX_INIMIGOS; ++j){ 
+          Inimigos[j].y += 1; 
         }
       }
     }
@@ -313,23 +293,23 @@ void PercursoInimigos(){ //movimentacao dos inimigos
 void PercursoTiros(){
   for (int i=0; i<MAX_TIROS; ++i){
     if (Tiros[i].Iniciado){
-      Tiros[i].y--; //simula o movimento de subida
-      if (Tiros[i].y <= MINY){ //verifica se o tiro esta ativo na tela ou n
+      Tiros[i].y--; 
+      if (Tiros[i].y <= MINY){ 
         Tiros[i].Iniciado = 0; 
       }
     }
   }
 }
 void Colisoes(){
-  for (int i=0; i<MAX_INIMIGOS; ++i){ // Verifica se as balas atingiram os inimigos
+  for (int i=0; i<MAX_INIMIGOS; ++i){ 
     if (Inimigos[i].Iniciado){
-      for (int j=0; j<MAX_TIROS; ++j){ //verifica se a posicao dos tiros do jgdr e da nave inimiga coincidem
+      for (int j=0; j<MAX_TIROS; ++j){ 
         if (Tiros[j].Iniciado && Tiros[j].y == Inimigos[i].y &&
             Tiros[j].x >= Inimigos[i].x &&
             Tiros[j].x <= Inimigos[i].x + LINIMIGO){
-          Inimigos[i].vida--; //Se um tiro atingir um inimigo, ele morre 
-          Tiros[j].Iniciado = 0; //seu tiro é desativado
-          if (Inimigos[i].vida <= 0){ //inimigo morto, ent é desativado
+          Inimigos[i].vida--; 
+          Tiros[j].Iniciado = 0; 
+          if (Inimigos[i].vida <= 0){ 
             Inimigos[i].Iniciado = 0;
             Score += 100; 
           }
@@ -337,10 +317,10 @@ void Colisoes(){
       }
     }
   }
-  for (int i=0; i<MAX_TIROS; ++i){ // Verifica se as balas atingiram a nave do jogador
-    if (Tiros[i].Iniciado && Tiros[i].y == Nave.y && Tiros[i].x >= Nave.x && Tiros[i].x <= Nave.x + LNAVE){ //verifica se estao na msm coordenada
+  for (int i=0; i<MAX_TIROS; ++i){ 
+    if (Tiros[i].Iniciado && Tiros[i].y == Nave.y && Tiros[i].x >= Nave.x && Tiros[i].x <= Nave.x + LNAVE){ 
       Tiros[i].Iniciado = 0; 
-      VidaJgdr--; // Reduz a vida do jogador quando atingido
+      VidaJgdr--; 
       if (VidaJgdr <= 0){
         printf("\033[1;31mGAME OVER\033[m");
         break;
@@ -349,35 +329,46 @@ void Colisoes(){
   }
 }
 void Colisoes2(){ 
-  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){ //percorre os tiros ativos p ver se atingiram o jgdr
-    if (TirosInimigos[i].Iniciado && //tiro esta ativo?
-        TirosInimigos[i].y == Nave.y && //verifica as coordenadas
+  for (int i=0; i<MAX_TIROS_INIMIGOS; ++i){ 
+    if (TirosInimigos[i].Iniciado && 
+        TirosInimigos[i].y == Nave.y && 
         TirosInimigos[i].x >= Nave.x &&
         TirosInimigos[i].x <= Nave.x + LNAVE){
-      VidaJgdr--; //se atingir, o jgdr perde vida
+      VidaJgdr--;
+      Score -= 50;
       TirosInimigos[i].Iniciado = 0;
-      if (VidaJgdr <= 0){ //jgdr morto
+      if (VidaJgdr <= 0){ 
         screenClear();
         screenGotoxy(MAXX / 2 - 4, MAXY / 2);
         printf("\033[1;31mGAME OVER\033[m");
         screenUpdate();
-        sleep(3); // Aguarda  segundos antes de encerrar o jogo
-        break;
+        sleep(3);
+        exit(0); 
       }
+    }
+  }
+  for (int i=0; i<MAX_INIMIGOS; ++i){
+    if (Inimigos[i].Iniciado && Inimigos[i].y + AINIMIGO >= MAXY){
+      screenClear();
+      screenGotoxy(MAXX / 2 - 4, MAXY / 2);
+      printf("\033[1;31mGAME OVER\033[m");
+      screenUpdate();
+      sleep(3);
+      exit(0); 
     }
   }
 }
 void Atirar(){ 
-  for (int i=0; i<MAX_TIROS; ++i){ //percorre o array de tiros
-    if (!Tiros[i].Iniciado){ //Verifica se um tiro específico não está iniciado
+  for (int i=0; i<MAX_TIROS; ++i){ 
+    if (!Tiros[i].Iniciado){ 
       Tiros[i].Iniciado = 1;
-      Tiros[i].x = Nave.x + LNAVE / 2; //posicao do tiro especifico
+      Tiros[i].x = Nave.x + LNAVE / 2; 
       Tiros[i].y = Nave.y - 1;
       break;
-    } //garante que so um tiro seja disparado por vez
+    } 
   }
 }
-int main() {
+int main(){
   screenInit(0);
   keyboardInit();
   timerInit(50);
@@ -388,11 +379,10 @@ int main() {
   VidaJgdr = VIDA_INICIAL;
   int ch = 0;
   int TiroInimigoTimer = 0;
-  int jogadorDigitouNome = 0; // Nova variável para verificar se o jogador já digitou seu nome
+  int jogadorDigitouNome = 0; 
 
-  carregarPontuacaoDoArquivo(); // Carregar pontuações do arquivo antes de iniciar o jogo
-
-  while (ch != 'q' && !jogadorDigitouNome) { // Verifica se o jogador não digitou o nome ainda
+  carregarPontuacaoDoArquivo();
+  while (ch != 'q' && !jogadorDigitouNome) { 
       drawBorders();
 
         if (timerTimeOver()) {
@@ -410,31 +400,25 @@ int main() {
             printf("\033[1;34mScore:\033[m \033[1;34m%d\033[m", Score);
             screenUpdate();
         }
-
-        if (InimigosDerrotados()) {
+        if (InimigosDerrotados()){
             screenClear();
             screenGotoxy(MAXX / 2 - 4, MAXY / 2);
-
             char nome[4];
-            printf("\033[1;32mYOU WIN!\nDigite seu nome (3 caracteres): ");
+            printf("\033[1;32mYOU WIN!\n\033[1;34mDigite seu nome (3 caracteres):\033[m ");
             scanf("%3s", nome);
-
             adicionarPontuacao(nome, Score);
             mostrarRanking();
             salvarPontuacaoEmArquivo();
-
-            jogadorDigitouNome = 1; // Define que o jogador digitou o nome
+            jogadorDigitouNome = 1; 
             screenUpdate();
-            sleep(3);
-    }
-
+            sleep(6);
+        }
         TiroInimigoTimer++;
-        if (TiroInimigoTimer >= 750) {
+        if (TiroInimigoTimer >= 900) {
             TiroInimigo();
             TiroInimigoTimer = 0;
         }
-
-        if (keyhit()) {
+        if (keyhit()){
             ch = readch();
             if (ch == ESQUERDA)
                 PercursoNave(-1);
@@ -444,8 +428,7 @@ int main() {
                 Atirar();
             }
         }
-
-        if (VidaJgdr <= 0) {
+        if (VidaJgdr <= 0){
             screenClear();
             screenGotoxy(MAXX / 2 - 4, MAXY / 2);
             screenUpdate();
@@ -454,8 +437,7 @@ int main() {
             ch = 'q';
         }
     }
-
-  liberarListaPontuacao(); // Liberar memória alocada para a lista de pontuações
+  liberarListaPontuacao(); 
   keyboardDestroy();
   screenDestroy();
   timerDestroy();
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Projeto de programação interativa e funcional</h1>
-<h2>Jogo em C - Arcade de Disparos :flying_saucer: :milky_way:</h2> <br>
+<h2>Jogo em C - Arcade de Disparos</h2> <br>
 
-<b>Grupo:</b> 2023.2- Turma A<br>
+<b>Grupo:</b> <br>
 Júlia Veríssimo - jov@cesar.school <br>
 Lucca da Veiga - lvg2@cesar.school <br>
 Ícaro Sampaio - isb2@cesar.school <br>
@@ -9,25 +9,3 @@ Lucca da Veiga - lvg2@cesar.school <br>
 <b>Professores:</b> <br>
 Pamela Bezerra; <br>
 Tiago Barros. <br>
-<hr>
-<h2>Instruções de compilação:</h2>
-Clone o repositório do GitHub que contém
-o código ou exporte os arquivos dele, manualmente.
-
-Comando para clonar: “ git clone https://github.com/Luketam/Projeto-em-C.git ”
-
-Abra os arquivos na sua máquina e para compilar o
-código, por linha de comando, escreva " make " no terminal
-do seu Linux ou Mac, e depois, “ ./programa ”.
-
-<b>OBS:</b>
-
-É necessário possuir um ambiente Linux ou MacOS para compilar o
-código, pois fizemos a utilização das bibliotecas CLI-LIB.
-
-Caso o usuário utilize Windows:
-Para rodar o código no VS Code, que foi a plataforma que
-utilizamos e recomendamos, é necessária a utilização do WSL,
-que permite a criação de um sistema Linux. Ao conectar o WSL
-ao VS Code, o usuário poderá rodar o código no Ubuntu com os
-comandos citados.
